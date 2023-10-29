71ff231949cba09fed00feee95609dd561f177c7
user <hag.hispin12@gmail.com>
2023-10-29 13:53:33 +0200

    version1

---
+++ app/src/main/java/com/amitai/mathprojectasafdadon/MainActivity.java
@@ -0,0 +1,36 @@
+package com.amitai.mathprojectasafdadon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button challenge;
+    private Button multi20;
+    private Button multiTable;
+    private Button check;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        etMain = findViewById(R.id.challege);
+        etMain = findViewById(R.id.text1);
+        etMain = findViewById(R.id.text3);
+        etMain = findViewById(R.id.multi20);
+        etMain = findViewById(R.id.multitable);
+    }
+
+public void clickOn(){
+
+}
+
+
+
+
+
+}
+
+
+
