4ee6ad980d1f44a457169517d70f1fea0a09b4ce
user <hag.hispin12@gmail.com>
2024-01-02 16:19:13 +0200

    task 7

--- app/src/main/java/com/amitai/mathprojectasafdadon/ShowUser.java
+++ app/src/main/java/com/amitai/mathprojectasafdadon/ShowUser.java
@@ -1,30 +1,73 @@
 package com.amitai.mathprojectasafdadon;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 
 public class ShowUser extends Fragment {
 
-
-
-
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
-
+MainViewModel model;
+Uri uri;
+private ImageView photo;
+private Button addPic;
+private Button addUser;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_show_user, container, false);
+        View view= inflater.inflate(R.layout.fragment_show_user, container, false);
+        photo = view.findViewById(R.id.photo);
+        addPic =view.findViewById(R.id.addPic);
+        main();
+        addUser= view.findViewById(R.id.addUser);
+
+        return  view;
     }
+
+    public void main(){
+        model = new ViewModelProvider(requireActivity()).get(MainViewModel.class);
+        ActivityResultLauncher<Intent> startCamera = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if (result.getResultCode()== RESULT_OK){
+                    photo.setImageURI(uri);
+                }
+            }
+        });
+
+        addPic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ContentValues values = new ContentValues();
+                values.put(MediaStore.Images.Media.TITLE, "New Picture");
+                values.put(MediaStore.Images.Media.DESCRIPTION, "From Camera");
+                uri = requireContext().getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+                startCamera.launch(cameraIntent);
+            }
+        });
+
+    }
+
+
+
 }
